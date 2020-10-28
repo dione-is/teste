82bc2a0917542ac18a643e3575a2ecab898db0fb
dione-is <73508786+dione-is@users.noreply.github.com>
2020-10-27 22:24:39 -0300

    Add files via upload

---
+++ funcao.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Funções</title>
+</head>
+<body>
+<script type="text/javascript">
+    var saldo = 1000;
+
+    saldo = saldo -10;
+    registro("debito", 10, "moises");
+
+    saldo = saldo -300;
+    registro("debito", 300, "Pedro");
+
+    saldo = saldo +800;
+    registro("credito", 800, "Ana");
+
+    function registro(operacao, valor, cliente) {
+        console.log("tipo"+operacao);
+        console.log("valor R$"+valor);
+        console.log("responsavel:"+cliente);
+        console.log("saldo atual:"+saldo);
+        console.log("-----------------------------------");
+        
+    }
+
+    </script>
+    
+</body>
+</html>
